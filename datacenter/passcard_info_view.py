39c6ab3810b6207ff4b540da943dc01fbd39cd15
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-05-31 10:15:08 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
+    passcard = Passcard.objects.all()
     # Программируем здесь
 
     this_passcard_visits = [
