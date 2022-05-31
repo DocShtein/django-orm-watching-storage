d2a5176fbaa745e8cc61b1f1d925f23a78497780
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-05-31 10:15:17 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,34 +1,18 @@
 import datetime
 
 from datacenter.models import Passcard
-from datacenter.models import Visit
-from django.utils.timezone import localtime
+from datacenter.models import Visit, get_duration, format_duration, is_visit_long
 from django.shortcuts import render
 
 
-def get_duration(visit):
-    all_employee_time_list = []
-    for local_time in visit:
-        local_entry_time = localtime(local_time.entered_at)
-        local_exit_time = localtime(local_time.leaved_at)
-        duration = local_exit_time - local_entry_time
-        all_employee_time_list.append(duration)
-    return all_employee_time_list
-
-
-def format_duration(duration):
-    days, seconds = duration.days, duration.seconds
-    hours = days * 24 + seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = (seconds % 60)
-    return hours, minutes, seconds
-
 
 def storage_information_view(request):
-    employee_visits = Visit.objects.all()
+    visits = Visit.objects.all()
+    active_visits = visits.filter(leaved_at=None)
+    print(is_visit_long(visits))
 
-    for employee in employee_visits:
-        time_duration = get_duration(employee_visits)
+    for employee in active_visits:
+        time_duration = get_duration(active_visits)
         for employee_time in time_duration:
             hours, minutes, seconds = format_duration(employee_time)
             duration = f'{hours} ч. {minutes} мин.'
