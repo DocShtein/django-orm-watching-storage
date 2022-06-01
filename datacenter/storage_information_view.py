6a9fb86dbbd67de6eee09d43268c200e0647fa9b
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-06-01 22:09:41 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -9,22 +9,24 @@ from django.shortcuts import render
 def storage_information_view(request):
     visits = Visit.objects.all()
     active_visits = visits.filter(leaved_at=None)
-    print(is_visit_long(visits))
 
-    for employee in active_visits:
-        time_duration = get_duration(active_visits)
-        for employee_time in time_duration:
-            hours, minutes, seconds = format_duration(employee_time)
-            duration = f'{hours} ч. {minutes} мин.'
+    for visit in active_visits:
+        time_duration = get_duration(visit)
+        hours, minutes, seconds = format_duration(time_duration)
+        duration = f'{hours} ч. {minutes} мин.'
+        flag = is_visit_long(visit, minutes=60)
         non_closed_visits = [
             {
-                'who_entered': employee.passcard.owner_name,
-                'entered_at': employee.entered_at,
+                'who_entered': visit.passcard.owner_name,
+                'entered_at': visit.entered_at,
                 'duration': duration,
+                'is_strange': flag
             }
         ]
 
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
+
     return render(request, 'storage_information.html', context)
+
