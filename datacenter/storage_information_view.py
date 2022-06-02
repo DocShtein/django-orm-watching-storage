a45b220ca3b75c6cf5975ad294c9c520a371c188
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-06-02 18:03:49 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,28 +1,29 @@
 import datetime
 
 from datacenter.models import Passcard
-from datacenter.models import Visit, get_duration, format_duration, is_visit_long
 from django.shortcuts import render
-
+from django.utils import timezone
+from datacenter.models import Visit, format_duration, is_visit_long
+from django.utils.timezone import localtime
 
 
 def storage_information_view(request):
     visits = Visit.objects.all()
     active_visits = visits.filter(leaved_at=None)
+    non_closed_visits = []
 
     for visit in active_visits:
-        time_duration = get_duration(visit)
-        hours, minutes, seconds = format_duration(time_duration)
-        duration = f'{hours} ч. {minutes} мин.'
-        flag = is_visit_long(visit, minutes=60)
-        non_closed_visits = [
-            {
+        entry_time = localtime(visit.entered_at)
+        now = localtime(timezone.now())
+        time_delta = (now - entry_time).total_seconds()
+
+        non_closed_visit = {
                 'who_entered': visit.passcard.owner_name,
-                'entered_at': visit.entered_at,
-                'duration': duration,
-                'is_strange': flag
+                'entered_at': localtime(visit.entered_at),
+                'duration': format_duration(time_delta),
+                'is_strange': is_visit_long(visit, minutes=60)
             }
-        ]
+        non_closed_visits.append(non_closed_visit)
 
     context = {
         'non_closed_visits': non_closed_visits,
