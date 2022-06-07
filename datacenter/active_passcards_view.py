0c0879c9f099a2d76caf8fc2f7f30c3fb9046796
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-06-07 18:12:15 +0300

    Update active_passcards_view.py

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 
 def active_passcards_view(request):
-    filtered_passcards = Passcard.objects.filter(is_active=True)
+    filtered_active_passcards = Passcard.objects.filter(is_active=True)
     context = {
-        'active_passcards': filtered_passcards,
+        'active_passcards': filtered_active_passcards,
     }
     return render(request, 'active_passcards.html', context)
